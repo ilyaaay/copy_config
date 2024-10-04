07dd4e1e6524c5f133efd13c9450da0bb1cf7396
Suhodolets Ilya <iliasuhodolets@gmail.com>
2024-10-04 19:05:02 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::{env::args, fs, io::Read};
+use std::{
+    env, fs,
+    io::{self, Read, Write},
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = args().skip(1).collect::<Vec<String>>();
+    let mut args = env::args();
 
-    if let (Some(x), Some(y)) = (args.first(), args.get(1)) {
-        let mut data = Vec::new();
-        fs::File::open(x)?.read_to_end(&mut data)?;
-        fs::write(y, data)?;
-    };
+    if let (Some(x), Some(y)) = (args.nth(1), args.nth(0)) {
+        let mut buf = Vec::new();
+        fs::File::open(x)?.read_to_end(&mut buf)?;
+        fs::File::options()
+            .write(true)
+            .open(y)?
+            .write_vectored(&[io::IoSlice::new(&buf[..])])?;
+    }
 
     Ok(())
 }
